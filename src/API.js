31bd7d7d499556c3d86bf629d39811da7794c592
Mathias Atugonza <atumathias20@gmail.com>
2025-04-11 23:01:27 +0300

    modified these files

--- src/API.js
+++ src/API.js
@@ -1,23 +1,20 @@
 import axios from 'axios';
 
+
 const API = axios.create({
-  baseURL: "http://jessymay.pythonanywhere.com/api",  // Base URL for your deployed backend
+  baseURL: "jessymay.pythonanywhere.com/",
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 API.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response) {
-      const status = error.response.status;
-      
-      if (status === 403 || status === 401) {
-        console.error("Session expired or unauthorized. Redirecting to login.");
-        window.location.href = "/login";
-      }
+    if (error.response && error.response.status === 403) {
+      console.error("Session expired. Redirecting to login.");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
